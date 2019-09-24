68f81ce9c4b54b41427b7797dfd61f9d382e809c
Егор Бычков <egorbi2406@gmail.com>
2019-09-24 06:18:48 +0300

    Gulp config improve

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,52 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
-function css_style(done) {
-    gulp.src('./src/scss/style.scss')
+let out = {
+    html: ["./out/"],
+    css: ["./out/css"],
+    img: ["./out/images"],
+    fonts: ["./out/fonts"]
+}
+
+function build_html() {
+    return gulp.src("./src/index.html")
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest(out.html));
+}
+
+function build_scss() {
+    return gulp.src('./src/scss/style.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(out.css));
+}
+
+function minify_img() {
+    return gulp.src("./src/images/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest(out.img));
+}
+
+function fonts_parse() {
+    return gulp.src("./src/fonts/*")
+        .pipe(gulp.dest(out.fonts));
+}
+
+function dev_scss() {
+    return gulp.src('./src/scss/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             errLogToConsole: true,
@@ -19,13 +62,12 @@ function css_style(done) {
             suffix: '.min'
         }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./out/css/'))
-
-    done();
+        .pipe(gulp.dest("./src/css"));
 }
 
-function watchSass() {
-    gulp.watch("./src/scss/**/*.scss", css_style);
+function watchScss() {
+    gulp.watch("./src/scss/**/*.scss", dev_scss);
 }
 
-exports.default = watchSass;
+exports.default = watchScss;
+exports.build = gulp.parallel(build_html, build_scss, fonts_parse, minify_img);
